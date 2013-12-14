8cfbaa021e6b42c18142fb971ddf0f70a16ccbae
Robert Williams <rob@r-williams.com>
2013-12-14 18:05:01 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raincoat::Application.config.secret_key_base = 'b293b9153995ab425398a517f9a3bd70d45944f4a1c786a466690ef10c463e27b2ee851190d8571c1cdc599e2a605add3f46cd43de0dfe62deb9e9e71935c46f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raincoat::Application.config.session_store :cookie_store, key: '_raincoat_session'
