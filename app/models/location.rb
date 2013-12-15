2eca89006c1f53c7de511572da5f8eecb48926bd
Robert Williams <rob@r-williams.com>
2013-12-15 16:20:29 +0000

    Delegation on location.

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,10 @@ class Location
 
   validates :name, presence: true
 
+  delegate :temp_c, 
+           :temp_f, 
+           :description, to: :weather, prefix: true
+
   def weather
     Rails.cache.fetch("weather_for_#{name}") {
       WorldWeather::Weather.new.get(name)
